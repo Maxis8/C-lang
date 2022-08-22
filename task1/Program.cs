d5072be64f964dd123409f00fce5ac90763b6148
Max <imv-1max@yandex.ru>
2022-08-23 01:09:48 +0500

    C# task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+        int sqr_b = Convert.ToInt32(Math.Pow(b, 2));     
+        if (a ==  sqr_b)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        { 
+            Console.WriteLine("No");
+        } 
