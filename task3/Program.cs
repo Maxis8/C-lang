764d5f1de8dbbb23e16d0c4945873535c349b550
Max <imv-1max@yandex.ru>
2022-08-23 10:01:22 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите чичло");
+
+int num = int.Parse(Console.ReadLine());
+int current = num * -1;
+while (current <= num)
+{
+   
+   Console.Write($"{current} " );
+   current++;
+}
+
+
